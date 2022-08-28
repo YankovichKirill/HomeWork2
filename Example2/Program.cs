2c872af61c9ac26baa58deb707b684324b3d03c0
YankovichKirill <kirill.best97@gmail.com>
2022-08-28 14:53:46 +0300

    SaveExample2

---
+++ Example2/Program.cs
@@ -0,0 +1,15 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter a number:");
+        int anyNumber = Convert.ToInt32(Console.ReadLine());
+        string anyNumberText = Convert.ToString(anyNumber);
+        if (anyNumberText.Length > 2){
+            Console.WriteLine("Third digit ->" + anyNumberText[2]);
+    }
+    else {
+        Console.WriteLine("-> No third digit");
+    }
+    }
+}       
