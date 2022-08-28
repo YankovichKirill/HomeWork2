8d7e4b5b42cdbef817cc861156f0c222fa455dde
YankovichKirill <kirill.best97@gmail.com>
2022-08-28 15:13:51 +0300

    SaveExample3

---
+++ Example3/Program.cs
@@ -0,0 +1,21 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number for the day of the week:");
+        int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+        void DayWeek (int dayNumber){
+          if (dayNumber == 6 || dayNumber == 7){
+            Console.WriteLine("(This day is day off) -> Yes");
+          }
+          else if (dayNumber < 1 || dayNumber > 7){
+            Console.WriteLine("It's not a day of the week");
+          }
+          else Console.WriteLine("(This day is not a day off) -> No"); 
+        }
+        
+        DayWeek(dayNumber);
+
+    }
+}
