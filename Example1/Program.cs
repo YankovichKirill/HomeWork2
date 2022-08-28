ccebd8fbc255701627cd74b10a5ab573df5e355b
YankovichKirill <kirill.best97@gmail.com>
2022-08-28 14:43:08 +0300

    SAveExample1

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -1,4 +1,10 @@
-﻿Console.Write("Enter three-digit number");
-int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
-string stringNumber = Convert.ToString(threeDigitNumber);
-Console.WriteLine("The second digit of this number->" + stringNumber[1]);
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter three-digit number");
+        int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+        string stringNumber = Convert.ToString(threeDigitNumber);
+        Console.WriteLine("The second digit of this number->" + stringNumber[1]);
+    }
+}
